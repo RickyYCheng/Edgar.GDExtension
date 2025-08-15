c1f36db62ff5a40695df2b54595cdc99672ecde0
ricky <ricky.y.c@outlook.com>
2025-08-16 04:40:55 +0800

    clean

--- src/edgar.interop.cpp/_csharp_global_helper.h
+++ src/edgar.interop.cpp/_csharp_global_helper.h
@@ -3,12 +3,7 @@
 
 extern "C" {
     void csharp_init_global(
-        void *gdprint, 
-        void *get_doorhandle_from_door_array,
-        void *get_byte_from_packed_byte_array,
-        void *get_vector2_from_packed_vector2_array,
-        void *get_int32_from_packed_int32_array,
-        void *add_layout_room
+        void *gdprint
     );
     
     void csharp_obj_free(void *csharp_obj_handle_Ptr);
--- src/edgar.interop.cpp/register_types.cpp
+++ src/edgar.interop.cpp/register_types.cpp
@@ -12,47 +12,13 @@ void gdprint(const char *str) {
     UtilityFunctions::print(str);
 }
 
-void *get_doorhandle_from_door_array(TypedArray<EdgarGodotGenerator> *array_Ptr, int idx) {
-    EdgarGodotGenerator *door = Object::cast_to<EdgarGodotGenerator>((*array_Ptr)[idx]);
-    return door->get_csharp_obj_handle();
-}
-
-char get_byte_from_packed_byte_array(PackedByteArray *array_Ptr, int idx) {
-    return (*array_Ptr)[idx];
-}
-
-void get_vector2_from_packed_vector2_array(PackedVector2Array *array_Ptr, int idx, float *_out_x, float *_out_y) {
-    auto v = (*array_Ptr)[idx];
-    *_out_x = v.x;
-    *_out_y = v.y;
-}
-
-int get_int32_from_packed_int32_array(PackedInt32Array *array_Ptr, int idx) {
-    return (*array_Ptr)[idx];
-}
-
-void add_layout_room(TypedArray<Dictionary> *rooms, const char16_t *room, bool is_corridor, int posx, int posy, int transformation, const char16_t *room_template) {
-    Dictionary dict;
-    dict["room"] = String(room);
-    dict["is_corridor"] = is_corridor;
-    dict["position"] = Vector2i(posx, posy);
-    dict["transformation"] = transformation;
-    dict["room_template"] = room_template;
-    rooms->push_back(dict);
-}
-
 void initialize_types(ModuleInitializationLevel p_level) {
     if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
         return;
     }
 
     csharp_init_global(
-        &gdprint, 
-        &get_doorhandle_from_door_array,
-        &get_byte_from_packed_byte_array,
-        &get_vector2_from_packed_vector2_array,
-        &get_int32_from_packed_int32_array,
-        &add_layout_room
+        &gdprint
     );
     ClassDB::register_class<EdgarGodotGenerator>();
 }
